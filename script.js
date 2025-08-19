aa2528dca77c8b99658a1b8704211f5263841f8f
CDRIE95 <jmezatorrejon@gmail.com>
2025-08-18 22:10:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Mensaje al cargar la página
-console.log("Página de Arquitectura Moderna cargada correctamente.");
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
 
-// Función para el formulario de contacto
-function enviarFormulario() {
-    let nombre = document.getElementById("nombre").value;
-    let email = document.getElementById("email").value;
-    alert("Gracias " + nombre + ", tu mensaje ha sido enviado.");
-    return false; // Evita que la página se recargue
-}
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
