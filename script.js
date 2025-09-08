b252c1bd0fe877bffbb168ebff3ff6ae22cbfd15
CDRIE95 <jmezatorrejon@gmail.com>
2025-09-07 19:26:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
-
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
-
 
 const links = document.querySelectorAll('.nav-links a');
 links.forEach(link => {
@@ -13,3 +11,24 @@ links.forEach(link => {
     navLinks.classList.remove('active');
   });
 });
+
+function fibonacci(n) {
+    if (n < 0) return "No válido";
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    let a = 0, b = 1, temp;
+    for (let i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+function calcularFibonacci() {
+    const numero = parseInt(document.getElementById("numero").value);
+    const resultado = fibonacci(numero);
+    document.getElementById("resultado").textContent = 
+        `El número Fibonacci en la posición ${numero} es: ${resultado}`;
+}
